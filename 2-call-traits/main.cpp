44736da8a0ee140cb7e2fb9ff7ce1fa046267bb7
Richard <legalize@xmission.com>
2024-02-14 14:00:34 -0700

    Add clarifying static_asserts to demonstrate call_traits

--- 2-call-traits/main.cpp
+++ 2-call-traits/main.cpp
@@ -23,9 +23,14 @@ void print(std::ostream &str, typename boost::call_traits<T>::param_type arg)
 template <typename T>
 using Parameter = typename boost::call_traits<T>::param_type;
 
-static_assert(std::is_same<const int, Parameter<int>>::value);
-static_assert(std::is_same<int *const, Parameter<int *>>::value);
-static_assert(std::is_same<const Person &, Parameter<Person>>::value);
+static_assert(std::is_same<const int, Parameter<int>>::value, "Pass an int as const int");
+static_assert(std::is_same<int *const, Parameter<int *>>::value, "Pass a pointer to int as int *const (constant pointer to int)");
+static_assert(std::is_same<const Person &, Parameter<Person>>::value, "Pass a Person as const Person &");
+static_assert(std::is_same<int &, Parameter<int &>>::value, "Pass a reference to an int as int &");
+
+static_assert(std::is_same<int &, boost::call_traits<int &>::reference>::value, "Don't take a reference of a reference");
+static_assert(std::is_same<int &, boost::call_traits<int>::reference>::value, "Take a reference on a simple type");
+static_assert(std::is_same<const Person &, boost::call_traits<Person>::const_reference>::value, "Take a const reference on a struct");
 
 int main()
 {
